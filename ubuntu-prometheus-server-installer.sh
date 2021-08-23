309751bd28b2fa6d8a1a5388f4fc4b2cfac206fe
Tyler Lindberg <39677147+zveroboy152@users.noreply.github.com>
2021-08-22 20:01:10 -0700

    Create ubuntu-prometheus-server-installer.sh

---
+++ ubuntu-prometheus-server-installer.sh
@@ -0,0 +1,17 @@
+sudo apt update -y
+cd /tmp
+wget https://github.com/prometheus/prometheus/releases/download/v2.27.1/prometheus-2.27.1.linux-amd64.tar.gz
+tar xvf prometheus-2.27.1.linux-amd64.tar.gz
+cd prometheus-2.27.1.linux-amd64
+sudo mkdir -p /etc/prometheus
+sudo mkdir -p /var/lib/prometheus
+sudo mv prometheus promtool /usr/local/bin/
+sudo mv consoles/ console_libraries/ /etc/prometheus/
+sudo mv prometheus.yml /etc/prometheus/prometheus.yml
+sudo groupadd --system prometheus
+sudo useradd -s /sbin/nologin --system -g prometheus prometheus
+sudo chown -R prometheus:prometheus /etc/prometheus/  /var/lib/prometheus/
+sudo chmod -R 775 /etc/prometheus/ /var/lib/prometheus/
+  
+sudo systemctl start prometheus
+sudo systemctl enable prometheus
