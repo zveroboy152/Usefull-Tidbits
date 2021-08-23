fea7dcdf76a600c1cadec24018bba851232e9e19
Tyler Lindberg <39677147+zveroboy152@users.noreply.github.com>
2021-08-22 23:53:30 -0700

    Update ubuntu-prometheus-node-explorer.sh

--- ubuntu-prometheus-node-explorer.sh
+++ ubuntu-prometheus-node-explorer.sh
@@ -4,5 +4,25 @@ cd /tmp
       mv node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/
       useradd -rs /bin/false node_exporter
       
-      /etc/systemd/system/node_exporter.service
+      sudo nano /etc/systemd/system/node_exporter.service[Unit]
+
+Description=Node Exporter
+
+After=network.target
+
+[Service]
+
+User=node_exporter
+
+Group=node_exporter
+
+Type=simple
+
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+
+WantedBy=multi-user.target
+      
+      
       systemctl start node_exporter
