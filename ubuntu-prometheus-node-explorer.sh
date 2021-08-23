43b3ac29b749f369012a878159d7263a7a55c8e1
Tyler Lindberg <39677147+zveroboy152@users.noreply.github.com>
2021-08-22 23:55:31 -0700

    Update ubuntu-prometheus-node-explorer.sh

--- ubuntu-prometheus-node-explorer.sh
+++ ubuntu-prometheus-node-explorer.sh
@@ -1,8 +1,8 @@
 cd /tmp
  wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
       tar -xvf node_exporter-1.0.1.linux-amd64.tar.gz
-      mv node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/
-      useradd -rs /bin/false node_exporter
+      sudo mv node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/
+      sudo useradd -rs /bin/false node_exporter
       
       sudo nano /etc/systemd/system/node_exporter.service
 
